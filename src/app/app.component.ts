2347e41e80f4c68b5db9ca08fc7a62b7ed1b7f43
FernandoAlv24 <100179088+FernandoAlv24@users.noreply.github.com>
2023-05-04 01:41:42 -0500

    Cambios de app terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+ dadoizq = '../assets/img/dice1.png';
+ dadoder= '../assets/img/dice4.png';
+ numero1 =1 ;
+ numero2 =2;
+
+ tirarDados():void{
+  this.numero1= Math.round(Math.random()*5+1);
+  this.numero2= Math.round(Math.random()*5+1);
+ this.dadoizq='../assets/img/dice'+ this.numero1+'.png'
+ this.dadoder='../assets/img/dice'+ this.numero2+'.png'
+ }
 }
